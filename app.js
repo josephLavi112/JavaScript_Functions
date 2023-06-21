3b8dfb07916783917aa981a4f5059ec805c01eb4
Joseph LaVia <josephdlavia@gmail.com>
2023-06-21 16:50:49 -0500

    JavaScriptVarXercise

--- app.js
+++ app.js
@@ -2,6 +2,24 @@ console.log("Hello World!\n==========\n");
 
 // Exercise 1 Section
 console.log("EXERCISE 1:\n==========\n");
+function printOdds(count) {
+    for(let i = 1; i <= count; i--) {
+        if (i % 2 !== 0) {
+          console.log(i);
+        }
+      }
+    }else{
+    for(let i = 1; i <= count; i++){
+        if(i % 2 !== 0){
+            console.log(i);
+        }
+    }
+  }
+}
 
+          
+
+  printOdds(10);
+  printOdds(-10);
 // Exercise 2 Section
 console.log("EXERCISE 2:\n==========\n");
